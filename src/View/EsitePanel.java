bd2386b147a504ca43cdd4eca78f20c395c5e788
Name-less <ph0enix.fiire@gmail.com>
2015-03-16 10:33:59 +0100

    article panel implemented. Not tested

--- src/View/EsitePanel.java
+++ src/View/EsitePanel.java
@@ -22,12 +22,42 @@ public class EsitePanel extends Panel {
             this.articlePanel.add(createArticlePanel(myArticles.get(i)));
         }
 
+        this.setLayout(new GridLayout(1,2));
+
+        this.add(articlePanel);
+        this.add(createPanierPanel());
 
     }
 
     private Panel createArticlePanel(Article article){
         Panel toReturn = new Panel(new GridBagLayout());
+        JLabel price = new JLabel(String.valueOf(article.getPrice()+"€"));
+        JLabel id = new JLabel(String.valueOf("Numéro : "+article.getId()));
+        JLabel name = new JLabel(article.getName());
+        JLabel description = new JLabel(article.getDescription());
+
         GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        toReturn.add(name,c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        toReturn.add(price,c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        toReturn.add(description,c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 2;
+        toReturn.add(id,c);
 
         return toReturn;
     }
